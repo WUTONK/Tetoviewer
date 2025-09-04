f00371dc07f66fca5d69e8822b598ce8228a4ccb
WUTONK <wjt474696120@gmail.com>
2025-09-04 18:31:04 +0800

    基本布局和icon

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,35 +1,93 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+// import Versions from './components/Versions'
+// import electronLogo from './assets/electron.svg'
+import React from 'react'
+import { Layout, Nav, Button, Breadcrumb, Skeleton, Avatar } from '@douyinfe/semi-ui';
+import { IconSemiLogo, IconBell, IconHelpCircle, IconBytedanceLogo, IconHome, IconLive, IconSetting } from '@douyinfe/semi-icons';
+import fatassTetoIcon from './assets/icon/fatassteto_pixel.png'
+import tetoPearIcon from './assets/icon/tetopear_pixel.png'
+import svTechieIcon from './assets/icon/SvTechie_pixel.png'
+import svTechieAndTetoPearIcon from './assets/icon/tetopearAndSvTechi.png'
 
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+  const body = document.body
+
+  const darkModeToggle = (): void => {
+    if (body.hasAttribute('theme-mode')) {
+      body.removeAttribute('theme-mode')
+    } else {
+      body.setAttribute('theme-mode', 'dark')
+    }
+  }
+
+  // JS/TS语法糖 相当于 const Header = Layout.Header
+  const { Header, Footer, Sider, Content } = Layout
+
+  const commonStyle = {
+    height: 64,
+    lineHeight: '64px'
+  }
 
   return (
     <>
-      {/* <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions> */}
+      {/* 逆天 这个居然是靠先后顺序改变布局的 字节跳动资本你赢了 */}
+      <Layout style={{ border: '1px solid var(--semi-color-border)', minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+        <Header style={{ ...commonStyle, background: 'rgba(var(--semi-pink-8), 1)' }}>
+          <div>
+            <Nav mode='horizontal' defaultSelectedKeys={['Home']} style={{...commonStyle,background: 'rgba(var(--semi-pink-1), 1)'}}>
+              <Nav.Header>
+                <img src={svTechieAndTetoPearIcon} alt="Logo" style={{ height: '32px' }}></img>
+              </Nav.Header>
+              <span
+                style={{
+                  color: 'var(--semi-color-text-2)',
+                }}
+              >
+                <span
+                  style={{
+                    marginRight: '24px',
+                    color: 'var(--semi-color-text-0)',
+                    fontWeight: '600',
+                  }}
+                >
+                  模版推荐
+                </span>
+                <span style={{ marginRight: '24px' }}>所有模版</span>
+                <span>我的模版</span>
+              </span>
+              <Nav.Footer>
+                <Avatar color="pink" size="small">
+                                  WUTONK
+                </Avatar>
+              </Nav.Footer>
+            </Nav>
+          </div>
+        </Header>
 
-      <p>hello world</p>
+        <Layout style={{ flex: 1, display: 'flex' }}>
+          <Sider style={{background: 'rgba(var(--semi-pink-6), 1)', }}>
+            <Nav
+              style={{ maxWidth: 200, height: '100%' }}
+              defaultSelectedKeys={['Home']}
+              items={[
+                { itemKey: 'Home', text: 'Home', icon: <IconHome size="large" /> },
+                { itemKey: 'fatass', text: 'fatass', icon: <img src={fatassTetoIcon} alt="fatass" style={{ width: '24px', height: '24px' }} /> },
+                { itemKey: 'meme', text: 'meme', icon: <img src={tetoPearIcon} alt='tetopear' style={{width: '24px', height:'30px'}} /> },
+                { itemKey: 'techie', text: 'techie', icon: <img src={svTechieIcon} alt='svTechie' style={{width: '30px', height: '50px'}} /> },
+              ]}
+              footer={{
+                collapseButton: true
+              }}
+            />
+          </Sider>
+
+          <Content style={{ background:"rgba(var(--semi-pink-2), 1)", flex: 1 }}>Content1</Content>
+        </Layout>
+        <Footer style={{ ...commonStyle, background: 'rgba(var(--semi-pink-3), 1)' }}>Footer</Footer>
+      </Layout>
+
+      {/* <Button onClick={ipcHandle}>Hello Semi</Button>
+      <Button onClick={darkModeToggle}>darkMode</Button> */}
     </>
   )
 }
